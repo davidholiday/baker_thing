537998b6356276599605b39b2cbddf9718e4b89c
snerd <davidholiday@users.noreply.github.com>
2018-04-06 23:32:36 -0600

    db comms seems to be working as well as basic user input validation and logging

--- flask_app_source/app.py
+++ flask_app_source/app.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from werkzeug.exceptions import BadRequest
@@ -23,7 +23,8 @@ class Customers(db.Model):
     """
     model defining customer records
     """
-    phone_number = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(42), nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
@@ -40,7 +41,7 @@ ROUTES
 """
 
 # constant defining what version to insert into all the api routes
-API_VERSION = 1
+API_AND_VERSION = '/api/v1'
 
 # landing page
 #
@@ -50,15 +51,33 @@ def hello_world():
     return 'Flask Dockerized'
 
 
-@app.route('/checkin', methods=['POST'])
+checkin_route = API_AND_VERSION + '/checkin'
+@app.route(checkin_route, methods=['POST'])
 def checkin():
     # there's a much more sophisticated library to handle this sort of thing - but it's out of scope for this exercise.
     # here we'll assume we're dealing with vanilla US numbers 555-555-5555 -- no special characters.
 
+
+    # validate caller supplied parameter(s)
+    #
+    phone_number = ""
     try:
         phone_number = str(request.json['phone_number'])
     except:
+        app.logger.error('missing phone_number key in payload or payload is not json')
+        raise BadRequest #TODO you're not going to use the built-in pages for this in the end...
+
+    if len(phone_number) != 10 or phone_number.isdigit() == False:
+        app.logger.error('phone_number must be ten digits with no special characters')
         raise BadRequest
+
+    # check to see if user is in db
+    #
+    customers_row_object = Customers.query \
+                                    .filter_by(phone_number=phone_number) \
+                                    .first()
+
+    app.logger.info('customers_row_object is: {}'.format(customers_row_object))
 
     return phone_number
 
