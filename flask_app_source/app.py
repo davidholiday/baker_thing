eda6a59c430a2d0e6c003852a05321d85c176bbb
snerd <davidholiday@users.noreply.github.com>
2018-04-08 00:05:00 -0600

    most of the acs are met

--- flask_app_source/app.py
+++ flask_app_source/app.py
@@ -1,5 +1,8 @@
 
 
+
+import pytz
+from datetime import datetime
 from flask import Flask, request, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
@@ -18,7 +21,6 @@ db = SQLAlchemy(app)
 Bootstrap(app)
 
 
-
 """
 DB MODEL(S)
 """
@@ -33,7 +35,7 @@ class Customers(db.Model):
     email = db.Column(db.String(120), nullable=False)
     points = db.Column(db.Integer, nullable=False, default=0)
     checkins = db.Column(db.Integer, nullable=False, default=0)
-    last_checkin = db.Column(db.DateTime) # TODO make sure all times are GMT!
+    last_checkin = db.Column(db.DateTime(timezone=True))
 
     def __repr__(self):
         return '<phone_number %r>' % self.phone_number
@@ -47,31 +49,28 @@ ROUTES
 # constant defining what version to insert into all the api routes
 API_AND_VERSION = '/api/v1'
 
-# landing page
-#
+
 @app.route('/')
-def hello_world():
+def root():
     return render_template('index.html')
 
 
-checkin_route = API_AND_VERSION + '/checkin'
-@app.route(checkin_route, methods=['POST'])
-def checkin():
-    # there's a much more sophisticated library to handle this sort of thing - but it's out of scope for this exercise.
-    # here we'll assume we're dealing with vanilla US numbers 555-555-5555 -- no special characters.
+registration_route = API_AND_VERSION + '/registration'
+@app.route(registration_route, methods=['POST'])
+def registration():
 
-    app.logger.info(request.form)
-    # validate caller supplied parameter(s)
+
+    # grab caller supplied parameters (relying on client to validate)
     #
     try:
-        phone_number = str(request.form['phone_number'])
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
     except:
-        app.logger.error('missing phone_number key in payload or payload is not form data')
+        app.logger.error('missing k,v pair in payload or payload is not form data')
         raise BadRequest
 
-    if len(phone_number) != 10 or phone_number.isdigit() == False:
-        app.logger.error('phone_number must be ten digits with no special characters')
-        raise BadRequest
 
     # check to see if user is in db
     #
@@ -80,7 +79,113 @@ def checkin():
                                     .first()
 
     app.logger.info('customers_row_object is: {}'.format(customers_row_object))
-    return render_template('index.html', message=phone_number, message_bg="bg-danger")
+
+
+    # build response
+    #
+
+    # create the record if they aren't already in the db
+    if customers_row_object == None:
+
+        timestamp = datetime.now(pytz.utc)
+
+        customers_row_object = Customers(first_name=first_name,
+                                         last_name=last_name,
+                                         email=email,
+                                         phone_number=phone_number,
+                                         points=50,
+                                         checkins=1,
+                                         last_checkin = timestamp)
+
+        db.session.add(customers_row_object)
+        db.session.commit()
+
+        message_bg = 'bg-info'
+
+        message = 'thanks for registering! ' \
+                  'you have checked in {} times, ' \
+                  'you have {} points, ' \
+                  'and your last checking was at {}'.format(1, 50, timestamp )
+
+        template = 'index.html'
+    # the user can't access the registration page directly, so if they accidentally enter someone else's phone number
+    # we need to tell them that.
+    # FIXME make it so the phone number is pre-populated and can't be altered to prevent this situation
+    else:
+        message_bg = 'bg-danger'
+        message = 'oops! someone is already registered with that number!'
+        template = 'index.html'
+
+    return render_template(template, message=message, message_bg=message_bg)
+
+
+
+
+checkin_route = API_AND_VERSION + '/checkin'
+@app.route(checkin_route, methods=['POST'])
+def checkin():
+
+
+    # grab caller supplied parameter(s) (relying on client to validate)
+    #
+    try:
+        phone_number = request.form['phone_number']
+    except:
+        app.logger.error('missing phone_number key in payload or payload is not form data')
+        raise BadRequest
+
+
+    # check to see if user is in db
+    #
+    customers_row_object = Customers.query \
+                                    .filter_by(phone_number=phone_number) \
+                                    .first()
+
+    app.logger.info('customers_row_object is: {}'.format(customers_row_object))
+
+
+    # build response
+    #
+    message_bg = 'bg-info'
+
+    if customers_row_object == None:
+        message = 'you must be new here - welcome! please register to start earning points.'
+        template = 'registration.html'
+    else:
+
+        # check time since last checkin (should be >5m)
+        timestamp_now = datetime.now(pytz.utc)
+        timestamp_last_checkin = customers_row_object.last_checkin
+        minutes_since_last_checkin = (timestamp_now - timestamp_last_checkin).total_seconds() / 60
+
+        if minutes_since_last_checkin >= 5:
+
+            # update points
+            customers_row_object.checkins += 1
+            customers_row_object.points += 20
+            customers_row_object.last_checkin = timestamp_now
+            db.session.commit()
+
+
+            # send email
+
+            message = 'w00t! you just earned 20 loyalty points! ' \
+                      'you have checked in {} times, ' \
+                      'you now have {} points, ' \
+                      'and your last checking was at {}'.format(customers_row_object.checkins,
+                                                                customers_row_object.points,
+                                                                timestamp_now)
+
+        else:
+            message_bg = 'bg-danger'
+
+            message = 'it has been {} minute(s) since you last checked in. ' \
+                      'you can only check in every five minutes!'.format(int(minutes_since_last_checkin))
+
+        template = 'index.html'
+
+    return render_template(template, message=message, message_bg=message_bg)
+
 
 
 """
